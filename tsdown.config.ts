09ea4ad2e1d871f0549a60932b612825f97d5f39
Leonardo Gurgel <leo@gurgel.io>
2026-06-02 22:13:32 -0300

    feat: improve configuration

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: "src/result.ts",
   outDir: "dist",
-  format: "esm",
   sourcemap: true,
   clean: true,
   dts: true,
+  exports: true,
+  platform: "neutral",
+  target: "es2022",
 });
